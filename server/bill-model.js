a5dc2de54ed0d146458a1e7fdf262b00dec88a76
Ernest <ernest.kies@codema.pl>
2022-05-29 16:19:09 +0200

    Name of the collection

--- server/bill-model.js
+++ server/bill-model.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
+const uuid = require("uuid");
 const Schema = mongoose.Schema;
 const billSchema = new Schema({
-  id: { type: Number, required: true, unique: true },
+  id: { type: String, default: uuid.v1(), required: true, unique: true },
   name: String,
-  saying: String
+  category: String,
+  cost: Number
+},
+{
+  collection: "Bills"
 });
 
 const Bill = mongoose.model("Bill", billSchema);
