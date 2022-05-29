0ba4ed8735c17faf4b771cf55c080029466d9fb3
Ernest <ernest.kies@codema.pl>
2022-05-29 22:57:23 +0200

    Create user/login routes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const cors = require("cors");
-const http = require('http');
 
 const index = require('./routes/index');
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('port', PORT);
 
 app.use(cors());
 app.use('/api', index);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const { registerUser, authUser } = require("../controllers/userControllers");
 const router = express.Router();
 
-const billService = require("../bill-service");
+const billService = require("../services/bill-service");
 
 router.get("/bills", async (req, res) => {
   billService.getBills(req, res);
 });
 
 router.post("/bill", async (req, res) => {
-  console.log(req.body)
   billService.postBill(req, res);
 })
+
+router.post("/users", registerUser);
+router.post("/login", authUser);
 
 router.get('/about', async (req, res) => {
   res.send({ title: "OK" });
